e1f05ac16a64d5a867fc2e8591b2567ce13226e0
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2024-02-18 10:42:02 +0100

    Moved controler

--- src/main/java/com/miaplatform/springstarter/controller/CPStatusController.java
+++ src/main/java/com/miaplatform/springstarter/controller/CPStatusController.java
@@ -1,4 +1,4 @@
-package com.miaplatform.springstarter.config.controller;
+package com.miaplatform.springstarter.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
